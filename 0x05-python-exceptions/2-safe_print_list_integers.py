82b16acb68833bf9088e0ba9ea8f5366ffde596e
stelmaks <stelmaks101@gmail.com>
2023-03-27 11:44:40 -0700

    Safe function

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    result = 0
-    for i in range(1, 3):
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
         try:
-            if i > a:
-                raise Exception('Too far')
-            else:
-                result += (a ** b) / i
-        except Exception:
-            result = b + a
-            break
-    return (result)
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    return (count)
