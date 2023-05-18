6c7dc290382b5dac138721800175e3df6540c30f
stelmaks <stelmaks@101@gmail.com>
2023-05-18 14:06:51 -0700

    My genres

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+-- Lists all shows contained in the database hbtn_0d_tvshows.
+ELECT tv_shows.title, tv_show_genres.genre_id
+	FROM tv_shows
+	LEFT JOIN tv_show_genres
+	ON tv_shows.id = tv_show_genres.show_id
+	LEFT JOIN tv_genres
+	ON tv_genres.id = tv_show_genres.genre_id
+	ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- Lists all genres of the show Dexter in the database hbtn_0d_tvshows.
+SELECT tv_genres.name
+	FROM tv_shows
+	JOIN tv_show_genres
+	ON tv_shows.id = tv_show_genres.show_id
+	JOIN tv_genres
+	ON tv_genres.id = tv_show_genres.genre_id
+	WHERE tv_shows.title = "Dexter"
+	ORDER BY tv_genres.name ASC;
