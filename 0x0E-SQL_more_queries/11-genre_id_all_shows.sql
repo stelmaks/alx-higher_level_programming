20a8037fa1116201869bee5e30a1b55a09d3fcba
stelmaks <stelmaks@101@gmail.com>
2023-05-18 14:20:50 -0700

    List shows and genres

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- Lists all shows contained in the database hbtn_0d_tvshows.
-ELECT tv_shows.title, tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 	FROM tv_shows
 	LEFT JOIN tv_show_genres
 	ON tv_shows.id = tv_show_genres.show_id
