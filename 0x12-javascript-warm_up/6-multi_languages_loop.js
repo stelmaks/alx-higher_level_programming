9ad9a5ce9d70583207531557c8bc51c9c7b7b7fa
stelmaks <stelmaks@101@gmail.com>
2023-06-15 14:57:18 -0700

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-// Basic print with Javascript
 
-const array = ['C is fun',
-  'Python is cool',
-  'Javascript is amazing'];
-let i = 0;
+// This script prints strings using a loop
 
-while (array[i]) {
-  console.log(array[i]);
-  i++;
+let i;
+const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (i = 0; i < 3; i++) {
+  console.log(myString[i]);
 }
