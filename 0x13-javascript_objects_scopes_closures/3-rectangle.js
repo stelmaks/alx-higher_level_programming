454988a6a6d27cabcb4692a832ad285e1cf22a97
stelmaks <stelmaks@101@gmail.com>
2023-06-14 17:09:40 -0700

    Rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,16 @@
+ h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const line = 'X'.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(line);
+    }
+  }
+}
+
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,1 @@
+Solutions to task 0x13. JavaScript - Objects, Scopes and Closures.
