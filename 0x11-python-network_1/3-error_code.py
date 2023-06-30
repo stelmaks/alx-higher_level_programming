adc8c5768d87ee11cd6962c06c7aba76229b4004
stelmaks <stelmaks@101@gmail.com>
2023-06-30 13:18:01 -0700

    Time for an interview!

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Request and display response body, including error codes
+"""
+
+import sys
+import urllib.error
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
